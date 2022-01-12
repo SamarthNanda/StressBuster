18d0d14a1e7d353f52cfbcd6faf2a24ec69ea055
SamarthNanda <samarthnanda1114@gmail.com>
2022-01-13 02:06:37 +0530

    make it better

--- src/component/drive/Drive.jsx
+++ src/component/drive/Drive.jsx
@@ -7,7 +7,7 @@ export default function Drive(){
         <div className="drive">
             <h3 className="and">And!</h3><br />
             <h3>While You Take A Break Lets Checkout Some Of Our  </h3>
-            <a href="https://drive.google.com/drive/folders/1ruNyjw0w8nzpneadfVrTh7Gc3FpYenpP?usp=sharing" target="_blank" rel="noopener noreferrer" className="drive-link">Memories.</a>
+            <a href="https://drive.google.com/drive/folders/1UeIEIgyVcr9KrZagVYrY2HumTaGigBYj?usp=sharing" target="_blank" rel="noopener noreferrer" className="drive-link">Memories.</a>
         </div>
     )
 }
